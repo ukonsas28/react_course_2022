f135e75731a2f0b9cecf01f6d26a0d8527e68892
Alexey Shirokov <shirokovam@lad24.ru>
2022-03-25 23:58:22 +0300

    fix

--- src/components/pages/OneUserPage/index.tsx
+++ src/components/pages/OneUserPage/index.tsx
@@ -2,11 +2,19 @@ import React from 'react';
 import { Navigate } from 'react-router-dom';
 import PageWrapper from '../../common/PageWrapper';
 
-const OneUserPage = ({ user }: any) => (
-  <>
-    <h1>OneUserPage</h1>
-    <h1>{user?.name}</h1>
-    <h1>{user?.age}</h1>
-  </>
-);
+const OneUserPage = ({ user }: any) => {
+  console.log(user);
+
+  if (!user) {
+    return <h1>NOT FOUND</h1>;
+  }
+
+  return (
+    <>
+      <h1>OneUserPage</h1>
+      <h1>{user?.name}</h1>
+      <h1>{user?.age}</h1>
+    </>
+  );
+};
 export default OneUserPage;
